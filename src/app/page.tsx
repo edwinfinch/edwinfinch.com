cdad1c3fe406cc3a3e37085c4fb557854ef0fc24
Edwin Finch <edwin@lignite.io>
2024-01-30 11:58:15 +0800

    Add draft header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,60 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <nav>
+        <p>nav bar here...</p>
+      </nav>
+      
+      {/* Header */}
+      <div className={styles.headerContainer}>
+        <h1>
+          <span>Hi, I&apos;m Edwin.</span>
+          <span>A developer.</span>
+        </h1>
+        <p>I&apos;m passionate about making challenging software projects happen. I&apos;ve run two Kickstarters, a software company, an Airbnb, a mental health event, and more.</p>
+      </div>
+
+      <div>
+        <h1>About me</h1>
+        <p>I am a Canadian who writes code, exercises and speaks Chinese.</p>
+      </div>
+      <div>
+        <h1>Projects</h1>
+        <p>Here are all of the projects I&apos;ve worked on.</p>
+        <ol>
+          <li>Lignite Collection for Pebble</li>
+          <li>Austin Spark League</li>
+          <li>And more...</li>
+        </ol>
+      </div>
+      <div>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit scelerisque mauris pellentesque pulvinar pellentesque. Elit pellentesque habitant morbi tristique senectus et netus et malesuada. Sollicitudin ac orci phasellus egestas tellus. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Luctus venenatis lectus magna fringilla urna. Vel eros donec ac odio tempor orci dapibus. Sit amet aliquam id diam. Cursus turpis massa tincidunt dui ut ornare lectus sit amet. Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Urna duis convallis convallis tellus id interdum. Dolor morbi non arcu risus quis.
+        </p><p>
+          Sit amet tellus cras adipiscing enim eu turpis egestas. Sem et tortor consequat id porta nibh. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Pretium quam vulputate dignissim suspendisse in est ante. Pretium quam vulputate dignissim suspendisse in. Euismod elementum nisi quis eleifend quam adipiscing vitae. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Nunc vel risus commodo viverra maecenas. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Sit amet venenatis urna cursus. Magnis dis parturient montes nascetur. Rhoncus mattis rhoncus urna neque viverra justo nec. In nisl nisi scelerisque eu ultrices vitae. Et ultrices neque ornare aenean euismod. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. In tellus integer feugiat scelerisque. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Accumsan lacus vel facilisis volutpat est. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia.
+        </p><p>
+          Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Magna fringilla urna porttitor rhoncus dolor purus. Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum. Sit amet consectetur adipiscing elit ut aliquam. Interdum varius sit amet mattis vulputate enim nulla aliquet. Lacus laoreet non curabitur gravida arcu ac tortor. Tempor nec feugiat nisl pretium fusce. Sodales ut eu sem integer. Euismod lacinia at quis risus sed vulputate.
+        </p><p>
+          Massa enim nec dui nunc mattis enim. Sit amet volutpat consequat mauris nunc. Tincidunt eget nullam non nisi est sit. Pellentesque dignissim enim sit amet venenatis. Morbi leo urna molestie at elementum eu. Sagittis vitae et leo duis ut diam quam. Sit amet mauris commodo quis imperdiet massa tincidunt. Pharetra magna ac placerat vestibulum. Non odio euismod lacinia at quis risus sed vulputate odio. Sed odio morbi quis commodo odio aenean. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Id velit ut tortor pretium viverra suspendisse.
+        </p><p>
+          Etiam tempor orci eu lobortis elementum. Morbi blandit cursus risus at ultrices mi tempus. Eget velit aliquet sagittis id consectetur purus ut faucibus. Nulla pharetra diam sit amet nisl suscipit adipiscing. Eget egestas purus viverra accumsan in nisl. Non consectetur a erat nam at. Ut porttitor leo a diam sollicitudin tempor id. Erat velit scelerisque in dictum. Laoreet sit amet cursus sit amet dictum sit amet. Sodales ut etiam sit amet nisl purus. Quam viverra orci sagittis eu volutpat odio facilisis. Etiam erat velit scelerisque in dictum. Dignissim sodales ut eu sem integer vitae. Nunc pulvinar sapien et ligula ullamcorper malesuada proin.
+        </p><p>
+          Magna eget est lorem ipsum dolor. Sit amet aliquam id diam maecenas ultricies mi eget mauris. Tincidunt lobortis feugiat vivamus at augue. Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. A condimentum vitae sapien pellentesque habitant morbi. Ut tristique et egestas quis. Vel pretium lectus quam id leo in vitae turpis. Vel eros donec ac odio tempor orci. Natoque penatibus et magnis dis parturient montes. Lectus mauris ultrices eros in. Libero volutpat sed cras ornare arcu dui vivamus arcu felis. Mattis aliquam faucibus purus in massa tempor nec feugiat. Est ultricies integer quis auctor elit. Nunc non blandit massa enim. Lobortis feugiat vivamus at augue eget. Elementum nisi quis eleifend quam adipiscing vitae. Semper viverra nam libero justo laoreet.
+        </p><p>
+          Praesent elementum facilisis leo vel. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Facilisis sed odio morbi quis commodo odio aenean sed. Mi sit amet mauris commodo quis. Risus sed vulputate odio ut enim blandit volutpat maecenas. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. A arcu cursus vitae congue mauris rhoncus. Commodo sed egestas egestas fringilla. Urna id volutpat lacus laoreet. Sed sed risus pretium quam vulputate dignissim suspendisse in. Elementum eu facilisis sed odio morbi quis commodo odio. Quisque id diam vel quam elementum pulvinar etiam non quam. Nunc scelerisque viverra mauris in aliquam sem fringilla ut. Quisque egestas diam in arcu cursus euismod. Lacus luctus accumsan tortor posuere ac ut. Praesent elementum facilisis leo vel fringilla. Iaculis nunc sed augue lacus viverra vitae. In nulla posuere sollicitudin aliquam ultrices. Imperdiet nulla malesuada pellentesque elit eget gravida.
+        </p><p>
+          Sapien eget mi proin sed libero enim sed faucibus turpis. Vel eros donec ac odio tempor orci dapibus ultrices in. At consectetur lorem donec massa sapien faucibus et molestie. Arcu vitae elementum curabitur vitae nunc sed. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Sed euismod nisi porta lorem. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Purus sit amet volutpat consequat. Vitae ultricies leo integer malesuada nunc vel risus commodo viverra. Ultrices mi tempus imperdiet nulla. Venenatis lectus magna fringilla urna porttitor rhoncus. Etiam dignissim diam quis enim lobortis scelerisque fermentum. Ut aliquam purus sit amet luctus venenatis lectus magna. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Tristique senectus et netus et malesuada fames ac turpis egestas. Senectus et netus et malesuada fames ac turpis egestas sed. Donec massa sapien faucibus et molestie ac feugiat sed lectus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget.
+        </p><p>
+          Venenatis a condimentum vitae sapien pellentesque habitant morbi. Porta lorem mollis aliquam ut porttitor leo. Habitant morbi tristique senectus et netus. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Non blandit massa enim nec dui nunc. Eu non diam phasellus vestibulum lorem sed. Blandit libero volutpat sed cras ornare arcu. Velit aliquet sagittis id consectetur purus. Semper quis lectus nulla at volutpat diam ut venenatis. Semper quis lectus nulla at volutpat. Mattis aliquam faucibus purus in massa. In ante metus dictum at tempor commodo ullamcorper a. Et malesuada fames ac turpis egestas integer eget aliquet. Eget nunc scelerisque viverra mauris in aliquam sem fringilla. Dignissim sodales ut eu sem integer vitae. Nec feugiat in fermentum posuere urna nec tincidunt. Adipiscing elit pellentesque habitant morbi tristique senectus et netus.
+        </p><p>
+          In nibh mauris cursus mattis molestie. Auctor elit sed vulputate mi sit amet mauris commodo quis. Fames ac turpis egestas sed tempus urna et. Integer eget aliquet nibh praesent tristique magna. Accumsan sit amet nulla facilisi. Eget gravida cum sociis natoque penatibus et magnis. Sed sed risus pretium quam vulputate dignissim suspendisse in est. In iaculis nunc sed augue lacus viverra. Bibendum at varius vel pharetra. Congue mauris rhoncus aenean vel elit scelerisque. Diam vel quam elementum pulvinar.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer>
+        <p>And here is my footer. Contact me.</p>
+      </footer>
     </main>
   );
 }
